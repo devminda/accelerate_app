632f967e5e27d693dc2d30c5be24f6f6ce4fcdf4
devminda <devmindaproducts@gmail.com>
2025-01-03 14:41:05 +0530

    added icons change

--- main.py
+++ main.py
@@ -11,12 +11,12 @@ def main():
     if "quiz_answers" not in st.session_state:
         st.session_state["quiz_answers"] = ["Red", "Blue", "Green", "Yellow"]
     # Set up pages
-    admin_page = st.Page("admin.py", title="Admin", icon=":material/add_circle:")
-    quiz_page = st.Page("quiz.py", title="Quiz", icon=":material/add_circle:")
-    cloud_page = st.Page("cloud.py", title="Word Cloud", icon=":material/add_circle:")
+    admin_page = st.Page("admin.py", title="Admin", icon=":material/home:")
+    quiz_page = st.Page("quiz.py", title="Quiz", icon=":material/help:")
+    cloud_page = st.Page("cloud.py", title="Word Cloud", icon=":material/cloud:")
 
     pg = st.navigation([admin_page, quiz_page, cloud_page])
-    st.set_page_config(page_title="Accelerate SL teach", page_icon=":material/edit:")
+    st.set_page_config(page_title="Accelerate SL teach", page_icon=":material/play_arrow:")
     pg.run()
 
 main()
